7b9e361f4f73af8c57e9d25539498bb67cda5b2c
CameronJulian <60965234+CameronJulian@users.noreply.github.com>
2020-03-17 14:19:29 +0200

    Add files via upload

---
+++ main/java/za/ac/cput/adp3/ListDemo.java
@@ -0,0 +1,34 @@
+package za.ac.cput.adp3;
+
+import java.util.*;
+
+public class ListDemo {
+    public static void main(String[] args)
+    {
+
+        List<Integer> l1 = new ArrayList<Integer>();
+        l1.add(0, 1); // adds 1 at 0 index
+        l1.add(1, 2); // adds 2 at 1 index
+        System.out.println(l1); // [1, 2]
+
+
+        List<Integer> l2 = new ArrayList<Integer>();
+        l2.add(1);
+        l2.add(2);
+        l2.add(3);
+
+
+        l1.addAll(1, l2);
+        System.out.println(l1);
+
+
+        l1.remove(1);
+        System.out.println(l1); // [1, 2, 3, 2]
+
+        System.out.println(l1.get(3));
+
+
+        l1.set(0, 5);
+        System.out.println(l1);
+    }
+}
